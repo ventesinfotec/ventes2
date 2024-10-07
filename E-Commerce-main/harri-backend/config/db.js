dedfae6e17249a90acd0db7ce3ca16666075939a
Sakshamgargj <gargsaksham092@gmail.com>
2024-10-07 18:05:02 +0530

    Update db.js

--- E-Commerce-main/harri-backend/config/db.js
+++ E-Commerce-main/harri-backend/config/db.js
@@ -5,7 +5,7 @@ const { secret } = require('./secret');
 mongoose.set('strictQuery', false);
 
 // it is local url 
-const DB_URL = 'mongodb://127.0.0.1:27017/hamart';
+const DB_URL = 'mongodb+srv://akshat:Akshat01@atlascluster.w2v7p.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster';
 // it is mongodb url
 const MONGO_URI = secret.db_url;
 
